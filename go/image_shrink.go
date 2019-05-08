7f9089d1c1a210c349ca2710f993188f264e5017
huahang <huahang@xiaomi.com>
2019-05-08 11:19:33 +0800

    add size and quality flags
    Signed-off-by: huahang <huahang@xiaomi.com>

--- go/image_shrink.go
+++ go/image_shrink.go
@@ -1,40 +1,40 @@
 package imageshrink
 
 import (
-	"fmt"
-	"os"
-	"path"
-	"path/filepath"
-	"runtime"
-	"strings"
-	"sync"
+  "fmt"
+  "os"
+  "path"
+  "path/filepath"
+  "runtime"
+  "strings"
+  "sync"
 )
 
 type Worker func (workerID int, imagePaths <-chan string, waitGroup *sync.WaitGroup)
 
 func ImageShrink(scanPath string, worker Worker)  {
-	var waitGroup sync.WaitGroup
-	imagePaths := make(chan string, 128)
-	numCPU := runtime.NumCPU()
-	waitGroup.Add(numCPU)
-	for i := 0; i < numCPU; i++ {
-		go worker(i, imagePaths, &waitGroup)
-	}
-	_ = filepath.Walk(scanPath, func(filePath string, fileInfo os.FileInfo, err error) error {
-		if err != nil {
-			fmt.Printf("[Error] Hit an error! " + err.Error() + "\n")
-			return err
-		}
-		if !fileInfo.Mode().IsRegular() {
-			return nil
-		}
-		ext := path.Ext(filePath)
-		if !strings.EqualFold(ext, ".jpeg") && !strings.EqualFold(ext, ".jpg") {
-			return nil
-		}
-		imagePaths <- filePath
-		return nil
-	})
-	close(imagePaths)
-	waitGroup.Wait()
+  var waitGroup sync.WaitGroup
+  imagePaths := make(chan string, 128)
+  numCPU := runtime.NumCPU()
+  waitGroup.Add(numCPU)
+  for i := 0; i < numCPU; i++ {
+    go worker(i, imagePaths, &waitGroup)
+  }
+  _ = filepath.Walk(scanPath, func(filePath string, fileInfo os.FileInfo, err error) error {
+    if err != nil {
+      fmt.Printf("[Error] Hit an error! " + err.Error() + "\n")
+      return err
+    }
+    if !fileInfo.Mode().IsRegular() {
+      return nil
+    }
+    ext := path.Ext(filePath)
+    if !strings.EqualFold(ext, ".jpeg") && !strings.EqualFold(ext, ".jpg") {
+      return nil
+    }
+    imagePaths <- filePath
+    return nil
+  })
+  close(imagePaths)
+  waitGroup.Wait()
 }
