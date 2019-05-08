a74302951777afd52253e3e13f98a560cf785e6b
huahang <huahang@xiaomi.com>
2019-05-08 11:19:33 +0800

    add size and quality flags
    Signed-off-by: huahang <huahang@xiaomi.com>

--- go/imageshrink/main.go
+++ go/imageshrink/main.go
@@ -1,39 +1,53 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"github.com/imageshrink/imageshrink/go"
-	"os"
 	"os/exec"
+	"strconv"
 	"sync"
 )
 
-func convertWorker(workerID int, imagePaths <-chan string, waitGroup *sync.WaitGroup) {
-	defer waitGroup.Done()
-	for imagePath := range imagePaths {
-		fmt.Printf("[Processing] %s\n", imagePath)
-		convert, err := exec.LookPath("convert")
-		if nil != err {
-			panic("[Fatal] " + err.Error())
-			return
-		}
+func workerImpl(workerID int, imagePaths <-chan string, size int, quality int, waitGroup *sync.WaitGroup) {
+  defer waitGroup.Done()
+	sizeString := strconv.Itoa(size)
+	qualityString := strconv.Itoa(quality)
+  for imagePath := range imagePaths {
+    fmt.Printf("[Processing] %s\n", imagePath)
+    convert, err := exec.LookPath("convert")
+    if nil != err {
+      panic("[Fatal] " + err.Error())
+      return
+    }
 		command := exec.Command(
-			convert,
-			"-resize" , "4096x4096", "-quality", "90", imagePath, imagePath)
-		err = command.Run()
-		if nil != err {
-			fmt.Printf("[Error] Failed to process image: %s, error: %s\n", imagePath, err.Error())
-			continue
-		}
+      convert,
+      "-resize" , sizeString +"x" + sizeString,
+      "-quality", qualityString,
+      imagePath, imagePath)
+    err = command.Run()
+    if nil != err {
+      fmt.Printf("[Error] Failed to process image: %s, error: %s\n", imagePath, err.Error())
+      continue
+    }
+  }
+}
+
+func buildWorker(size int, quality int) imageshrink.Worker {
+	return func (workerID int, imagePaths <-chan string, waitGroup *sync.WaitGroup) {
+		workerImpl(workerID, imagePaths, size, quality, waitGroup)
 	}
 }
 
 func main()  {
-	args := os.Args
-	if len(args) != 2 {
-		fmt.Printf("Usage: imageshrink [path to scan]\n")
-		return
-	}
-	imageshrink.ImageShrink(args[1], convertWorker)
-	return
+	size := flag.Int("size", 4096, "image size")
+  quality := flag.Int("quality", 90, "image quality (1~100)")
+  flag.Parse()
+  args := flag.Args()
+  if len(args) != 1 {
+    fmt.Printf("Usage: imageshrink [path to scan]\n")
+    return
+  }
+  imageshrink.ImageShrink(args[0], buildWorker(*size, *quality))
+  return
 }
