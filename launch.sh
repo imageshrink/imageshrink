6242f571b135ca638798d24450035e2fea06162f
hans <hans@dkmt.io>
2022-05-08 01:12:11 +0800

    fix docker scripts
    Signed-off-by: hans <hans@dkmt.io>

--- launch.sh
+++ launch.sh
@@ -1,16 +1,7 @@
 #!/bin/sh
 
-docker kill watchtower
 docker kill imageshrink_server
-
-docker system prune
-
-docker run -d \
-  --name watchtower \
-  --restart always \
-  -v /var/run/docker.sock:/var/run/docker.sock \
-  containrrr/watchtower imageshrink_server --interval 60
-
+docker rm imageshrink_server
 docker run -d \
   --name imageshrink_server \
   --restart always \
