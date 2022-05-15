98a385e6d29bc6804fbbbff5e13c060c969df200
huahang <huahang@xiaomi.com>
2022-05-15 16:27:12 +0800

    wip
    Signed-off-by: huahang <huahang@xiaomi.com>

--- build.sh
+++ build.sh
@@ -1,6 +1,13 @@
 #!/bin/sh
 
-GOARCH="amd64" GOOS="linux" go build server/imageshrink_server.go
-mv imageshrink_server docker/imageshrink_server
-docker build docker -t imageshrink/imageshrink_server
-docker push imageshrink/imageshrink_server
+# GOARCH="amd64" GOOS="linux" go build server/imageshrink_server.go
+# mv imageshrink_server docker/imageshrink_server
+# docker build docker -t docker.io/imageshrink/imageshrink_server:manifest-amd64 --build-arg ARCH=amd64
+# docker push docker.io/imageshrink/imageshrink_server:manifest-amd64
+
+# GOARCH="arm64" GOOS="linux" go build server/imageshrink_server.go
+# mv imageshrink_server docker/imageshrink_server
+# docker build docker -t :manifest-arm64v8 --build-arg ARCH=arm64v8
+# docker push docker.io/imageshrink/imageshrink_server:manifest-arm64v8
+
+docker buildx build -t docker.io/imageshrink/imageshrink_server --push --platform="linux/arm64,linux/amd64" docker
